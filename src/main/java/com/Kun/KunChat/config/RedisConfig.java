150c8caa92810c981ae864c4744b21cdb9d0630a
Kun <yukun.zk@foxmail.com>
2025-04-20 14:31:27 +0800

    修改序列化方案，添加 CacheManager 用于 @Cacheable 实现不同数据类型的不同有效时间

--- src/main/java/com/Kun/KunChat/config/RedisConfig.java
+++ src/main/java/com/Kun/KunChat/config/RedisConfig.java
@@ -1,6 +1,11 @@
 package com.Kun.KunChat.config;
 
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +14,12 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 /**
@@ -24,11 +30,12 @@ import java.time.Duration;
  * Description:
  **/
 
+@EnableCaching
 @Configuration
 public class RedisConfig {
 
     // 使用 Jackson2JsonRedisSerializer 来序列化和反序列化对象
-    private final Jackson2JsonRedisSerializer<Object> jacksonSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+    private final FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
 
     // String 序列化
     private final StringRedisSerializer serializer = new StringRedisSerializer();
@@ -38,13 +45,20 @@ public class RedisConfig {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
 
+        // LocalDate序列化
+        ObjectMapper mapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        // 设置序列化格式
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        mapper.registerModule(javaTimeModule);
+
         // 设置 key 和 value 的序列化方式
         redisTemplate.setKeySerializer(serializer); // key 使用 String 序列化
-        redisTemplate.setValueSerializer(jacksonSerializer); // value 使用 Jackson2Json 序列化
+        redisTemplate.setValueSerializer(fastJsonRedisSerializer); // value 使用 Jackson2Json 序列化
 
         // 设置 hash 的 key 和 value 序列化方式
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(jacksonSerializer);
+        redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);
 
         return redisTemplate;
     }
@@ -54,40 +68,48 @@ public class RedisConfig {
         return redisTemplate.opsForHash();
     }
 
+    @Bean("CacheManager_Nomal")
+    public CacheManager cacheManager1(RedisConnectionFactory redisConnectionFactory) {
+
+        // 配置自动缓存的序列化
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofHours(1)).serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer)).serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(fastJsonRedisSerializer)).disableCachingNullValues();
+
+        return RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(config).build();
+    }
+
+    // 用于设置用户的缓存时间
+    @Bean("CacheManager_User")
+    public CacheManager cacheManager2(RedisConnectionFactory redisConnectionFactory) {
+
+        // 配置自动缓存的序列化
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(7)).serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer)).serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(fastJsonRedisSerializer)).disableCachingNullValues();
+
+        return RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(config).build();
+    }
+
     @Bean("KeyGenerator")
     public KeyGenerator keyGenerator() {
         return (target, method, params) -> {
             StringBuilder sb = new StringBuilder();
             // 类名
-            String className = target.getClass().getName() + ":";
+            String className = target.getClass().getSimpleName() + ":";
             // 方法名
             String methodName = method.getName() + ":";
             // 字符拼接
-            sb.append("RedisAutoCache:");
             sb.append(className);
             sb.append(methodName);
             // 请求参数的拼接
             for (Object obj : params) {
                 sb.append(obj.toString());
-                sb.append("&");
+                int i = 0;
+                if (params.length - i != 1) {
+                    sb.append("&");
+                }
+                i++;
             }
             return sb.toString();
         };
 
     }
 
-    @Bean("CacheManager")
-    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-
-        // 配置自动缓存的序列化
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofHours(1))
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jacksonSerializer))
-                .disableCachingNullValues();
-
-        return RedisCacheManager.builder(redisConnectionFactory)
-                .cacheDefaults(config)
-                .build();
-    }
 }
