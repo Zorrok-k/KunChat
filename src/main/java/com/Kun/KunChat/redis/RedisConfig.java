e8f4f469001c08429a6d07a85284c96d2c5f8612
Kun <yukun.zk@foxmail.com>
2025-04-20 12:37:12 +0800

    增加 cacheManager 配置自动缓存时的序列化和其他配置

--- src/main/java/com/Kun/KunChat/redis/RedisConfig.java
+++ src/main/java/com/Kun/KunChat/redis/RedisConfig.java
@@ -1,13 +1,19 @@
 package com.Kun.KunChat.redis;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 
 /**
@@ -21,16 +27,19 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    // 使用 Jackson2JsonRedisSerializer 来序列化和反序列化对象
+    private final Jackson2JsonRedisSerializer<Object> jacksonSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
+    // String 序列化
+    private final StringRedisSerializer serializer = new StringRedisSerializer();
+
     @Bean("RedisTemplate")
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
 
-        // 使用 Jackson2JsonRedisSerializer 来序列化和反序列化对象
-        Jackson2JsonRedisSerializer<Object> jacksonSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-
         // 设置 key 和 value 的序列化方式
-        redisTemplate.setKeySerializer(new StringRedisSerializer()); // key 使用 String 序列化
+        redisTemplate.setKeySerializer(serializer); // key 使用 String 序列化
         redisTemplate.setValueSerializer(jacksonSerializer); // value 使用 Jackson2Json 序列化
 
         // 设置 hash 的 key 和 value 序列化方式
@@ -65,5 +74,20 @@ public class RedisConfig {
             return sb.toString();
         };
 
+    }
+
+    @Bean("CacheManager")
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+
+        // 配置自动缓存的序列化
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jacksonSerializer))
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(config)
+                .build();
     }
 }
