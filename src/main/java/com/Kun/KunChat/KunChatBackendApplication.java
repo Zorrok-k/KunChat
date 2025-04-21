2fc8a7be91d1e1359783007ad6b29d4474084275
Kun <yukun.zk@foxmail.com>
2025-04-21 19:06:16 +0800

    小修改

--- src/main/java/com/Kun/KunChat/KunChatBackendApplication.java
+++ src/main/java/com/Kun/KunChat/KunChatBackendApplication.java
@@ -10,10 +10,10 @@ public class KunChatBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KunChatBackendApplication.class, args);
-        System.out.println("------------------------------------------------------------------------------------");
-        System.out.println("                   Application：KunChat 服务端 正在运行！                             ");
-        System.out.println("                   Test Url：http://localhost:8680/api/                             ");
-        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("----------------------------------------------------------------------------------------------------------------\n");
+        System.out.println("                                 Application：KunChat 服务端 正在运行！\n                                         ");
+        System.out.println("                                 Test Url：http://localhost:8680/api/\n                                         ");
+        System.out.println("----------------------------------------------------------------------------------------------------------------");
     }
 
 
