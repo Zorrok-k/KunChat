8fdd5878ddcfba74834571b40ca858e100c1621e
Kun <yukun.zk@foxmail.com>
2025-04-23 19:43:00 +0800

    增加文件上传静态路径类

---
+++ src/main/java/com/Kun/KunChat/StaticVariable/StaticVariable.java
@@ -0,0 +1,32 @@
+package com.Kun.KunChat.StaticVariable;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Author: Beta
+ * Date: 2025/4/23 16:36
+ * Description: 文件路径静态变量
+ **/
+
+@Component
+public class StaticVariable {
+
+    // 用户静态资源路径
+    public static String USER_PATH = null;
+    public static final String USER_AVATAR_NAME = "avatar.jpg";
+    public static final String USERINFO_COVER_NAME = "userinfo_cover.jpg";
+    // 群组静态资源路径
+    public String GROUP_PATH = null;
+    public static final String GROUP_COVER_NAME = "group_cover.jpg";
+
+    StaticVariable() {
+    }
+
+    @Value("${spring.web.resources.static-locations}")
+    private void init(String basePath) {
+        USER_PATH = basePath.replace("file:", "") + "Users/";
+        GROUP_PATH = basePath.replace("file:", "") + "Groups/";
+    }
+
+}
