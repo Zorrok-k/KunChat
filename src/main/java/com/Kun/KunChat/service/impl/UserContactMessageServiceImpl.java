0ac94b2aa6b369c75952fcf302c3994ea380acf3
Kun <yukun.zk@foxmail.com>
2025-04-24 09:56:45 +0800

    再次初始化，开始写这三张表的业务了🥲

---
+++ src/main/java/com/Kun/KunChat/service/impl/UserContactMessageServiceImpl.java
@@ -0,0 +1,22 @@
+package com.Kun.KunChat.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.Kun.KunChat.entity.UserContactMessage;
+import com.Kun.KunChat.service.UserContactMessageService;
+import com.Kun.KunChat.mapper.UserContactMessageMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author Kun
+* @description 针对表【user_contact_message】的数据库操作Service实现
+* @createDate 2025-04-24 09:55:44
+*/
+@Service
+public class UserContactMessageServiceImpl extends ServiceImpl<UserContactMessageMapper, UserContactMessage>
+    implements UserContactMessageService{
+
+}
+
+
+
+
