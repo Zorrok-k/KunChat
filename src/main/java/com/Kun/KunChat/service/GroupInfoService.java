edb778a18ab02a1258ec90471070a7d0b6ad8bf1
Kun <yukun.zk@foxmail.com>
2025-04-24 14:55:40 +0800

    创建群组功能业务

--- src/main/java/com/Kun/KunChat/service/GroupInfoService.java
+++ src/main/java/com/Kun/KunChat/service/GroupInfoService.java
@@ -4,10 +4,12 @@ import com.Kun.KunChat.entity.GroupInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
-* @author Kun
-* @description 针对表【group_info】的数据库操作Service
-* @createDate 2025-04-24 10:27:46
-*/
+ * @author Kun
+ * @description 针对表【group_info】的数据库操作Service
+ * @createDate 2025-04-24 10:27:46
+ */
 public interface GroupInfoService extends IService<GroupInfo> {
+
+    GroupInfo createGroup(String userId, String groupName, int joinType);
 
 }
--- src/main/java/com/Kun/KunChat/service/impl/GroupInfoServiceImpl.java
+++ src/main/java/com/Kun/KunChat/service/impl/GroupInfoServiceImpl.java
@@ -1,20 +1,44 @@
 package com.Kun.KunChat.service.impl;
 
+import com.Kun.KunChat.common.CustomizeUtils;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.Kun.KunChat.entity.GroupInfo;
 import com.Kun.KunChat.service.GroupInfoService;
 import com.Kun.KunChat.mapper.GroupInfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
-* @author Kun
-* @description 针对表【group_info】的数据库操作Service实现
-* @createDate 2025-04-24 10:27:46
-*/
-@Service
-public class GroupInfoServiceImpl extends ServiceImpl<GroupInfoMapper, GroupInfo>
-    implements GroupInfoService{
+import java.time.LocalDateTime;
 
+/**
+ * @author Kun
+ * @description 针对表【group_info】的数据库操作Service实现
+ * @createDate 2025-04-24 10:27:46
+ */
+@Service
+public class GroupInfoServiceImpl extends ServiceImpl<GroupInfoMapper, GroupInfo> implements GroupInfoService {
+
+    @Autowired
+    private GroupInfoMapper groupInfoMapper;
+
+    @Autowired
+    private CustomizeUtils customizeUtils;
+
+    @Override
+    public GroupInfo createGroup(String userId, String groupName, int joinType) {
+        GroupInfo groupInfo = new GroupInfo();
+        groupInfo.setGroupId(customizeUtils.getUUID());
+        groupInfo.setGroupName(groupName);
+        groupInfo.setOwnerId(userId);
+        groupInfo.setJoinType(joinType);
+        // 生成当前时间
+        LocalDateTime timeNow = LocalDateTime.now();
+        groupInfo.setCreateTime(timeNow);
+        // 写入数据库
+        groupInfoMapper.insert(groupInfo);
+
+        return groupInfoMapper.selectById(groupInfo.getGroupId());
+    }
 }
 
 
