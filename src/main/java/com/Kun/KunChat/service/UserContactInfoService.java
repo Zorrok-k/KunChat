666618c9e4c706aa70a5908b1f13a3acbd941ff6
Kun <yukun.zk@foxmail.com>
2025-04-24 12:01:30 +0800

    MP 不能用联合主键，加包有点麻烦，我选择改数据库了🥲

---
+++ src/main/java/com/Kun/KunChat/service/UserContactInfoService.java
@@ -0,0 +1,13 @@
+package com.Kun.KunChat.service;
+
+import com.Kun.KunChat.entity.UserContactInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author Kun
+* @description 针对表【user_contact_info】的数据库操作Service
+* @createDate 2025-04-24 11:59:43
+*/
+public interface UserContactInfoService extends IService<UserContactInfo> {
+
+}
