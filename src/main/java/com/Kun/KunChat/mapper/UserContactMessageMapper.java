331b7995be2d8fbe0027d4ad96c02dffdbf99a6e
Kun <yukun.zk@foxmail.com>
2025-04-26 11:39:07 +0800

    TMD 每次都要生成两遍第一次总是错的路径 😡

--- src/main/java/com/Kun/KunChat/mapper/UserContactMessageMapper.java
+++ src/main/java/com/Kun/KunChat/mapper/UserContactMessageMapper.java
@@ -6,7 +6,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 /**
 * @author Kun
 * @description 针对表【user_contact_message】的数据库操作Mapper
-* @createDate 2025-04-26 11:33:40
+* @createDate 2025-04-26 11:38:06
 * @Entity com.Kun.KunChat.entity.UserContactMessage
 */
 public interface UserContactMessageMapper extends BaseMapper<UserContactMessage> {
--- src/main/java/com/Kun/KunChat/service/impl/UserContactMessageServiceImpl.java
+++ src/main/java/com/Kun/KunChat/service/impl/UserContactMessageServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 /**
 * @author Kun
 * @description 针对表【user_contact_message】的数据库操作Service实现
-* @createDate 2025-04-26 11:33:40
+* @createDate 2025-04-26 11:38:06
 */
 @Service
 public class UserContactMessageServiceImpl extends ServiceImpl<UserContactMessageMapper, UserContactMessage>
