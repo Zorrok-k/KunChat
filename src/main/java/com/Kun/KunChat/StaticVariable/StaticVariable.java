55c7445198b54791d52b4db6b1ff7466a9ac5390
Kun <yukun.zk@foxmail.com>
2025-05-04 20:00:12 +0800

    相应增加静态路径

--- src/main/java/com/Kun/KunChat/StaticVariable/StaticVariable.java
+++ src/main/java/com/Kun/KunChat/StaticVariable/StaticVariable.java
@@ -12,6 +12,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class StaticVariable {
 
+    // 静态资源路径
+    public static String BASE_PATH = null;
+
     // 用户静态资源路径
     public static String USER_PATH = null;
     public static final String USER_AVATAR_NAME = "avatar.jpg";
@@ -20,14 +23,19 @@ public class StaticVariable {
     public static String GROUP_PATH = null;
     public static final String GROUP_AVATAR_NAME = "avatar.jpg";
     public static final String GROUP_COVER_NAME = "group_cover.jpg";
+    // 聊天文件静态资源路径、网络路径
+    public static String CHATFILES_PATH = null;
+    public static final String CHATFILES_SRC = "/ChatFiles/";
 
     StaticVariable() {
     }
 
     @Value("${spring.web.resources.static-locations}")
     private void init(String basePath) {
-        USER_PATH = basePath.replace("file:", "") + "Users/";
-        GROUP_PATH = basePath.replace("file:", "") + "Groups/";
+        BASE_PATH = basePath.replace("file:", "");
+        USER_PATH = BASE_PATH + "Users/";
+        GROUP_PATH = BASE_PATH + "Groups/";
+        CHATFILES_PATH = BASE_PATH + "ChatFiles/";
     }
 
 }
